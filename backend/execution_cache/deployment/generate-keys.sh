1ad71bfbdc650d8a1ccd9a69ea949cd560e1ec96
rui5i <fang.ru@husky.neu.edu>
2020-02-13 09:38:18 -0800

    Test

---
+++ backend/execution_cache/deployment/generate-keys.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Generate a (self-signed) CA certificate and a certificate and private key to be used by the execution cache server.
+# The certificate will be issued for the Common Name (CN) of `execution-cache-server.webhook-demo.svc`, which is the
+# cluster-internal DNS name for the service.
+
+: ${1?'missing key directory'}
+
+key_dir="$1"
+
+chmod 0700 "$key_dir"
+cd "$key_dir"
+
+# Generate the CA cert and private key
+openssl req -nodes -new -x509 -keyout ca.key -out ca.crt -subj "/CN=Admission Controller Webhook Demo CA"
+# Generate the private key for the webhook server
+openssl genrsa -out webhook-server-tls.key 2048
+# Generate a Certificate Signing Request (CSR) for the private key, and sign it with the private key of the CA.
+openssl req -new -key webhook-server-tls.key -subj "/CN=webhook-server.webhook-demo.svc" \
+    | openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -out webhook-server-tls.crt
